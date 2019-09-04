502531129fb1c085c99096b4a86b31f3ab14cc92
Jake Bernstein <jgbernstein@gmail.com>
2019-09-04 12:44:36 -0400

    updated readbin.m to convert DCS to single, should halve the memory footprint

--- Utils/readbin.m
+++ Utils/readbin.m
@@ -12,7 +12,7 @@ function DCS = readbin(filename,imagesize,DCSperimage)
     end
     tmprawdat = fread(fid,Inf,'uint16');
     tmprawdat(tmprawdat == 2^12 - 1) = NaN;
-    tmprawdat = tmprawdat - 2^11;
+    tmprawdat = single(tmprawdat - 2^11);
     DCS = permute(reshape(tmprawdat',imagesize(2),imagesize(1),DCSperimage),[2 1 3]);
     fclose(fid);
 end
